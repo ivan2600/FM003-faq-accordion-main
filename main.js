e519cf80e85e9efd2564f28de16cc8357259ab65
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-12-20 23:01:50 -0300

    challenge finished

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-// https://futurecodersweb.com/faq-accordion-section-using-html-css-and-javascript/
+const question  = document.querySelectorAll('.question');
+const pregunta      = document.querySelectorAll('.pregunta');
+
+// Cuando CLICK en h2,
+  // QUITAR la clase activo de TODOS los bloque
+  // Vamos a añadir la clase activo al BLOQUE con la POSICION del h2
+
+
+// Recorresr TODOS los h2  
+pregunta.forEach( ( cadaPregunta , i )=>{
+  // Asignando un CLICK a cada h2
+  pregunta[i].addEventListener('click', ()=>{
+    // Recorrer TODOS los bloque
+    question.forEach( ( cadaQuestion , i )=>{
+      // Quitamos la clase activo de TODOS los bloque
+      question[i].classList.remove('activo')
+    })
+    // Añadiendo la clase activo al bloque cuya posición sea igual al del h2
+    // (Linea número 10)
+    question[i].classList.add('activo')
+
+  })
+}) 
