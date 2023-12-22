de14fa2a63eaf026791b58f4ea30769a260a5115
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-12-21 21:47:15 -0300

    logica de apertura y cierre finalizada

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
-const question  = document.querySelectorAll('.question');
-const pregunta      = document.querySelectorAll('.pregunta');
+const question    = document.querySelectorAll('.question');
+const pregunta    = document.querySelectorAll('.pregunta');
+const activo      = document.querySelectorAll('.activo')
 
-// Cuando CLICK en h2,
-  // QUITAR la clase activo de TODOS los bloque
-  // Vamos a añadir la clase activo al BLOQUE con la POSICION del h2
-
-
-// Recorresr TODOS los h2  
+// Recorrer TODOS los pregunta  
 pregunta.forEach( ( cadaPregunta , i )=>{
-  // Asignando un CLICK a cada h2
+  // Asignando un CLICK a cada pregunta
   pregunta[i].addEventListener('click', ()=>{
-    // Recorrer TODOS los bloque
-    question.forEach( ( cadaQuestion , i )=>{
-      // Quitamos la clase activo de TODOS los bloque
+    // Si el question CONTIENE la clase activo
+    if (question[i].classList.contains('activo')) {
+      // Borrar la clase activo
       question[i].classList.remove('activo')
-    })
-    // Añadiendo la clase activo al bloque cuya posición sea igual al del h2
-    // (Linea número 10)
-    question[i].classList.add('activo')
-
+    } else {
+      // Si no la contiene, agregarla.
+      question[i].classList.add('activo')
+    }
+        
   })
 }) 
